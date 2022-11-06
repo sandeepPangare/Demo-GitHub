98183fdfeca8b86de8e7cc56a00f362433060146
sandeepPangare <pangaresandeep96@gmail.com>
2022-11-06 10:46:05 +0530

    Added:HelloWorldJava

--- src/com/bridgelabz/HelloWorld.java
+++ src/com/bridgelabz/HelloWorld.java
@@ -3,6 +3,6 @@ package com.bridgelabz;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World java 2");
     }
 }
